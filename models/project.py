ad5825f0bdb0bc91051426a6adecabde47274128
Edgar <edgar.naranjo>
2021-11-25 02:06:31 +0100

    Add address

--- models/project.py
+++ models/project.py
@@ -7,6 +7,12 @@ class Project(models.Model):
     _name = "project.project"
 
     doc_count = fields.Integer(compute='_compute_attached_docs_count', string="Number of documents attached")
+    street = fields.Char()
+    street2 = fields.Char()
+    zip = fields.Char(change_default=True)
+    city = fields.Char()
+    state_id = fields.Many2one("res.country.state", string='State', ondelete='restrict', domain="[('country_id', '=?', country_id)]")
+    country_id = fields.Many2one('res.country', string='Country', ondelete='restrict')
 
     def _compute_attached_docs_count(self):
         Attachment = self.env['ir.attachment']
